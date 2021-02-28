7298fabb1302039032183137142990c682aeb28d
popotanu <scrapmacaron@gmail.com>
2021-02-28 19:05:19 +0900

    2: guessing game
    - match
    - type cast
    - shadowing
    - a simple error hundling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,20 +8,30 @@ fn main() {
 
     println!("the secret number is: {}", secret_number);
 
-    println!("lets input your guess, tanu.");
+    loop {
+        println!("lets input your guess, tanu.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
 
-    println!("you guessed: {} tanu!", guess);
+        // shadowing
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("too small!"),
-        Ordering::Greater => println!("too big!"),
-        Ordering::Equal => println!("tanu----!!!"),
+        println!("you guessed: {} tanu!", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => {
+                println!("tanu----!!!");
+                break;
+            }
+        }
     }
-
 }
